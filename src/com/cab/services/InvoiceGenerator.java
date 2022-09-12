5d7789958d0e0d320d0566c05a7624ff0dd427d3
Rakeshmadu <rakeshmadu24@gmail.com>
2022-09-12 11:16:14 +0530

    calculate multiple rides

--- src/com/cab/services/InvoiceGenerator.java
+++ src/com/cab/services/InvoiceGenerator.java
@@ -6,7 +6,21 @@ public class InvoiceGenerator {
 
 	public double generateInvoice(Ride ride) {
 		double fare = 10*ride.distance + ride.time;
-		
+
 		return fare > 5 ? fare : 5;
 	}
+
+	public double generateInvoice(Ride[] rides) {
+		double singleFare;
+		double totalFare = 0;
+		for (Ride ride : rides) {
+			singleFare = 10*ride.distance + ride.time;
+			totalFare += singleFare > 5 ? singleFare : 5;
+		}
+
+
+		return totalFare;
+	}
+
+
 }
--- src/com/cab/test/InvoiceTest.java
+++ src/com/cab/test/InvoiceTest.java
@@ -1,29 +1,39 @@
 package com.cab.test;
 
-import static org.junit.Assert.assertEquals;
-
+import com.cab.entities.Ride;
+import com.cab.services.InvoiceGenerator;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.cab.entities.Ride;
-import com.cab.services.InvoiceGenerator;
+import static org.junit.Assert.assertEquals;
 
 public class InvoiceTest {
-	
+
 	InvoiceGenerator invoice;
-	@Before 
+
+	@Before
 	public void initialization() {
 		invoice = new InvoiceGenerator();
 	}
-	
+
 	@Test
 	public void testGenerateInvoice() {
-		
+
 		assertEquals(103, invoice.generateInvoice(new Ride(10, 3)), 0.0);
 	}
+
 	@Test
 	public void testGenerateInvoice_getMin() {
-		
+
 		assertEquals(5, invoice.generateInvoice(new Ride(0.1, 1)), 0.0);
 	}
+
+	@Test
+	public void testGenerateInvoice_multipleRides() {
+
+		Ride [] rides = {new Ride(0.1, 2), new Ride(10, 3)};
+
+		assertEquals(108, invoice.generateInvoice(rides), 0.0);
+	}
+
 }
